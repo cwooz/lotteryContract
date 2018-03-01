706af4624547c387d73fba947e1540caeb80736e
cwooz <woosley.christopher@gmail.com>
2018-02-28 20:36:40 -0500

    update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -17,7 +17,7 @@ contract Lottery {
         return uint(keccak256(block.difficulty, now, players));
     }
     
-    function pickWinner() public restricted{
+    function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
         players = new address[](0);
