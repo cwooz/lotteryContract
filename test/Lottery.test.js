e8635853702ebc4a53c0c0296d907387a1c9fbfd
cwooz <woosley.christopher@gmail.com>
2018-02-28 21:26:53 -0500

    test4

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -71,4 +71,15 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lotter.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
