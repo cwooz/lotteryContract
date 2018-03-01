685b8d775f1c67840448e161dfe79705cab3d655
cwooz <woosley.christopher@gmail.com>
2018-02-28 20:46:09 -0500

    first test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -10,8 +10,13 @@ let accounts;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-
   lottery = await web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
 });
